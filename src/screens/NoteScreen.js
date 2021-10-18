c6cca3c9aaf7b80a79eb838dc789e3d88cb68f0d
Johan Melin <soldierheart@hotmail.com>
2021-10-18 16:28:38 +0200

    added note functionality

--- src/screens/NoteScreen.js
+++ src/screens/NoteScreen.js
@@ -1,17 +1,42 @@
 import React from 'react'
-import { StyleSheet, View, FlatList, TextInput } from 'react-native'
+import { StyleSheet, View, FlatList, TextInput, TouchableOpacity, Text } from 'react-native'
 
 const NoteScreen = () => {
     const [input, setInput] = React.useState("");
+    const [notes, setNotes] = React.useState(["test"]);
+
+    const addNote = () => {
+        setNotes(notes => [input, ...notes]);
+        setInput("");
+    }
+
+    const renderItem = ({ item }) => (
+        <View style={styles.bubble}>
+            <Text>{ item }</Text>
+        </View>
+    );
 
     return (
         <View style={styles.container}>
-            <FlatList style={styles.list} />
-            <TextInput
-                style={styles.input}
-                onChangeText={setInput}
-                value={input}
+            <FlatList 
+                contentContainerStyle={styles.list} 
+                data={notes}
+                extraData={notes}
+                renderItem={renderItem}
             />
+            <View style={styles.inputArea}>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setInput}
+                    value={input}
+                />
+                <TouchableOpacity 
+                    style={styles.addButton}
+                    onPress={addNote}
+                >
+                <Text style={{fontSize: 18, color: "#000"}}>+</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
@@ -24,6 +49,12 @@ const styles = StyleSheet.create({
     },
     list: {
         flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "flex-end",
+        margin: 10
+    },
+    inputArea: {
+        flexDirection: 'row'
     },
     input: {
         height: 40,
@@ -31,6 +62,25 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         padding: 10,
         borderRadius: 20,
-        flex: 0
+        flex: 1
     },
+    addButton: {
+        height: 40,
+        width: 40,
+        marginVertical: 12,
+        marginRight: 12,
+        borderWidth: 1,
+        borderRadius: 20,
+        flex: 0,
+        backgroundColor: "#39CCCC",
+        justifyContent: "center",
+        alignContent: "center",
+        alignItems: "center"
+    },
+    bubble: {
+        borderWidth: 1,
+        borderRadius: 10,
+        margin: 5,
+        padding: 5
+    }
 })
